b0682a42a9133efe9d3137b8ba39007f60118d1d
SeokYoung <willy4202@naver.com>
2022-04-13 17:23:09 +0900

    Add: 개인 파일 세팅 완료

--- src/pages/seokyoung/Login/Login.js
+++ src/pages/seokyoung/Login/Login.js
@@ -1,7 +1,62 @@
-import React from "react";
+import "../Login/Login.scss";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 function Login() {
-  return <div>Login</div>;
+  const navigate = useNavigate();
+  const goToMain = e => {
+    if (!values.text.includes("@")) {
+      alert("이메일 형식이 아닙니다.");
+    } else if (values.password.length <= 8) {
+      alert("비밀번호는 8자리 이상이여야 합니다.");
+    } else {
+      alert("로그인 성공!");
+      navigate("/main-seokyoung");
+    }
+  };
+
+  const [values, setValues] = useState({ text: "", password: "" });
+
+  const checkIdPw = e => {
+    setValues({ ...values, [e.target.type]: e.target.value });
+  };
+
+  const active = values.text && values.password;
+
+  return (
+    <div className="Login">
+      <section className="mainBox">
+        <h1 className="logo">westagram</h1>
+        <div id="inputBox" className="inputBox">
+          <form className="form" onChange={checkIdPw}>
+            <input
+              id="inputId"
+              className="inputUserInfo"
+              type="text"
+              placeholder="전화번호, 사용자 이름 또는 이메일"
+            />
+            <input
+              id="inputPass"
+              className="inputUserInfo"
+              type="password"
+              placeholder="비밀번호"
+            />
+          </form>
+          <input
+            id="loginBtn"
+            className={!active ? "loginBtn" : "activeLogin"}
+            type="button"
+            value="로그인"
+            disabled={!active}
+            onClick={goToMain}
+          />
+        </div>
+        <a className="forgotPassword" href="main.html">
+          비밀번호를 잊으셨나요?
+        </a>
+      </section>
+    </div>
+  );
 }
 
 export default Login;
--- src/pages/seokyoung/Main/Main.js
+++ src/pages/seokyoung/Main/Main.js
@@ -1,7 +1,103 @@
-import React from "react";
+import "../Main/Main.scss";
+import Nav from "../../../components/Nav/Nav";
 
 function Main() {
-  return <div>Main</div>;
+  return (
+    <div className="Main">
+      <Nav />
+      <main className="main">
+        <div className="feedMain">
+          <article className="feedPost">
+            <div className="feedTop">
+              <img src="images/seokyoung/feed.jpeg" alt="" />
+              <div className="feedTopName">heaundea_DDol</div>
+            </div>
+            <img className="feedImg" src="images/seokyoung/ddol.JPG" alt="" />
+            <div className="commentBox">
+              <div className="commentLogoBox">
+                <img
+                  className="navLogo"
+                  src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/heart.png"
+                  alt="좋아요"
+                />
+                <img
+                  className="navLogo"
+                  src="images/seokyoung/chat.png"
+                  alt="채팅"
+                />
+                <img
+                  className="navLogo"
+                  src="images/seokyoung/send.png"
+                  alt="메시지"
+                />
+                <img
+                  className="navLogo rightLogo"
+                  src="images/seokyoung/bookmark-white.png"
+                  alt="저장"
+                />
+              </div>
+              <div className="likes">좋아요 623개</div>
+              <div className="commentId">heaundea_DDol</div>
+              <p className="commentContent">똘이 너무 귀엽죠?</p>
+              <p className="comments">댓글 271개 모두 보기</p>
+              <ul className="commentPostList" />
+              <form className="commentInputBox">
+                <img src="images/seokyoung/smile.png" alt="이모티콘" />
+                <input
+                  className="commentInput"
+                  type="text"
+                  placeholder="댓글 달기"
+                />
+                <button className="commentBtn">게시</button>
+              </form>
+            </div>
+          </article>
+        </div>
+        <div className="feedRight">
+          <div className="feedRightProfile">
+            <img src="images/seokyoung/profile.JPG" alt="프로필" />
+            <div className="profileName">
+              syoung___h
+              <div className="profileKor">석영</div>
+            </div>
+            <div className="profileChange">전환</div>
+          </div>
+          <div className="feedRightRecommendBox">
+            <div className="RecommendText1">회원님을 위한 추천</div>
+            <div className="RecommendText2">모두 보기</div>
+          </div>
+          <div className="recommendFrineds">
+            <div className="freinds">
+              <div className="freind1">
+                <img src="images/seokyoung/freind1.png" alt="친구1" />
+                <div className="profileName">
+                  wecode_bootCamp
+                  <div className="profileKor">위코드 32기</div>
+                </div>
+                <div className="profileChange">팔로우</div>
+              </div>
+              <div className="freind1">
+                <img src="images/seokyoung/freind2.png" alt="친구2" />
+                <div className="profileName">
+                  finda_offical
+                  <div className="profileKor">핀다</div>
+                </div>
+                <div className="profileChange">팔로우</div>
+              </div>
+              <div className="freind1">
+                <img src="images/seokyoung/friend3.png" alt="친구3" />
+                <div className="profileName">
+                  kakao_developer
+                  <div className="profileKor">카카오</div>
+                </div>
+                <div className="profileChange">팔로우</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default Main;

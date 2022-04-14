208ecac1b706a8079cd86f0959ac37b11b67f1ab
HyunSuk <lha3421@gmail.com>
2022-04-14 14:19:34 +0900

    Add : Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/hyunsuk/Login/Login.js
+++ src/pages/hyunsuk/Login/Login.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 import { Link, useNavigate } from "react-router-dom";
 
 const Login = props => {
+  // const [bgColor, setBgColor] = useState({ backgroundColor: "#c0dffd" });
+  // const [disabled, setDisabled] = useState(true);
+  // const btnOnAndOff = () => {
+  //   if (inputId && inputPw) {
+  //     setDisabled(false);
+  //     setBgColor({ backgroundColor: "blue" });
+  //   }
+  // };
+  const [inputId, setInputId] = useState();
+  const [inputPw, setInputPw] = useState();
+  const handleInput = event => {
+    setInputId(event.target.value);
+    setInputPw(event.target.value);
+  };
+
   const navigate = useNavigate();
   const moveToMain = () => {
     navigate("/main-hyunsuk");
   };
+
   return (
     <div className="Login">
       <div className="LoginWrapper">
@@ -14,18 +30,35 @@ const Login = props => {
         <div className="loginDiv">
           <form action="#">
             <input
+              className="userName"
               required
               type="text"
               placeholder="전화번호, 사용자 이름 또는 이메일"
               id="id"
+              // onChange={e => {
+              //   setInputId(e.target.value);
+              //   btnOnAndOff();
+              // }}
+              onChange={handleInput}
             />
             <input
+              className="userPw"
               required
               type="password"
               placeholder="비밀번호"
               id="password"
+              // onChange={e => {
+              //   setInputPw(e.target.value);
+              //   btnOnAndOff();
+              // }}
+              onChange={handleInput}
             />
-            <button id="button" onClick={moveToMain}>
+            <button
+              id="button"
+              onClick={moveToMain}
+              // style={bgColor}
+              disabled
+            >
               로그인
             </button>
             <a href="#">비밀번호를 잊으셨나요?</a>

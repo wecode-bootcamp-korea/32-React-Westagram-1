930d9f521204edb8061f898366987096b10294b9
Lee-Ha Young <98532217+Rumisister@users.noreply.github.com>
2022-04-14 14:27:17 +0900

    Add: Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/hayoung/Login/Login.js
+++ src/pages/hayoung/Login/Login.js
@@ -1,13 +1,20 @@
-import React from "react";
+/* eslint-disable */
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import "./Login.scss";
 
 const Login = () => {
   const navigate = useNavigate();
+  const [userId, setUserId] = useState("");
+  const [userPw, setUserPw] = useState("");
 
   const goToMain = () => {
     navigate("/main-hayoung");
+  };
+
+  const handlePwInput = e => {
+    setUserPw(e.target.value);
   };
 
   return (
@@ -18,8 +25,16 @@ const Login = () => {
           type="text"
           className="inputId"
           placeholder="전화번호, 사용자 이름 또는 이메일"
+          value={userId}
+          onChange={e => setUserId(e.target.value)}
         />
-        <input type="password" className="inputPw" placeholder="비밀번호" />
+        <input
+          type="password"
+          className="inputPw"
+          placeholder="비밀번호"
+          value={userPw}
+          onChange={handlePwInput}
+        />
         <button className="login_btn" onClick={goToMain}>
           로그인
         </button>

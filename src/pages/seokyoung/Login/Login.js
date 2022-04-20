20efd43791a83410f78d0c254d4b58eb31b7138d
SeokYoung <willy4202@naver.com>
2022-04-20 16:57:38 +0900

    Add: Backend연결 완료

--- src/pages/seokyoung/Login/Login.js
+++ src/pages/seokyoung/Login/Login.js
@@ -4,15 +4,8 @@ import { useState } from "react";
 
 function Login() {
   const navigate = useNavigate();
-  const goToMain = e => {
-    if (!values.text.includes("@")) {
-      alert("이메일 형식이 아닙니다.");
-    } else if (values.password.length <= 8) {
-      alert("비밀번호는 8자리 이상이여야 합니다.");
-    } else {
-      alert("로그인 성공!");
-      navigate("/main-seokyoung");
-    }
+  const goToMain = () => {
+    navigate("/main-seokyoung");
   };
 
   const [values, setValues] = useState({ text: "", password: "" });
@@ -21,7 +14,23 @@ function Login() {
     setValues({ ...values, [e.target.type]: e.target.value });
   };
 
-  const active = values.text && values.password;
+  const active = values.text.includes("@") && values.password.length >= 8;
+
+  const signUp = () => {
+    fetch("http://10.58.1.201:8000/users/signin", {
+      method: "POST",
+      body: JSON.stringify({
+        email: values.text,
+        password: values.password,
+      }),
+    })
+      .then(res => res.json())
+      .then(result => {
+        result.message === "SUCCESS"
+          ? localStorage.setItem("token", result.access_token)
+          : alert("nop");
+      });
+  };
 
   return (
     <div className="Login">
@@ -48,7 +57,8 @@ function Login() {
             type="button"
             value="로그인"
             disabled={!active}
-            onClick={goToMain}
+            onClick={signUp}
+            // onClick={goToMain}
           />
         </div>
         <a className="forgotPassword" href="main.html">
